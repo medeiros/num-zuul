e4ca9c0c6e72326fe75324a46f4a65905bd1224a
Daniel Medeiros <daniel@arneam.com>
2019-11-01 06:04:36 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Num: Zuul
+
+Simple Zuul Gateway server for 'Num' solution.
---
+++ src/test/java/com/arneam/numzuul/NumZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arneam.numzuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NumZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
